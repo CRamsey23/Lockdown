ac7ed54384f546aaec97074c475c997b1af9455d
Chris Ramsey <chris.ramsey@fireeye.com>
2016-04-11 15:30:19 -0700

    Selected main color palette (http://paletton.com/#uid=34H0u0kllllaFw0g0qFqFg0w0aF)

--- js/lockdown.js
+++ js/lockdown.js
@@ -18,9 +18,32 @@ com.lockdown.model = (function() {
 })();
 
 com.lockdown.view = (function() {
+	var class_game_container = "game-container";
+
+	var class_color_primary = "color-primary-0";
+	var class_color_secondary = "color-secondary-1-0";
+	var class_color_tertiary = "color-secondary-2-0";
+
+	var class_background_color_primary = "background-color-primary-0";
+	var class_background_color_secondary = "background-color-secondary-1-0";
+	var class_background_color_tertiary = "background-color-secondary-2-0";
+
 	return {
+
+		gameframe: function() {
+			var gameframe_html = "<div class=\"" + [class_game_container,class_background_color_primary].join(" ") + "\">";
+
+
+			return gameframe_html + "</div>";
+		},
+
+
 		init: function(){
 			console.log("view");
+
+			var HTML = this.gameframe();
+
+			$("body").html(HTML);
 		}
 	};
 })();
